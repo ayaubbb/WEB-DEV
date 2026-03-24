65df8dbef64680ff4476f968993f5367084648ea
Ayaulym <ayaulym@bbb.com>
2026-03-25 01:56:25 +0500

    lab-8

--- lab-8/api/models.py
+++ lab-8/api/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
         
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     count = models.IntegerField()
     is_active = models.BooleanField()
