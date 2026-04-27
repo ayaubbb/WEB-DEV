71ccdd746b16cde0e9eb9f94b4228186892da2c7
Ayaulym <ayaulym@bbb.com>
2026-04-28 02:15:02 +0500

    g

---
+++ lab-8-copy/api/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            }
+        
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category_id': self.category.id
+            }
